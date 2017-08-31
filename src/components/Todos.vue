464e5537492604929167dbcf0727ecf0bc0c39f0
Seaborn Lee <seabornlee.cn@gmail.com>
2017-09-01 07:32:07 +0800

    Remove todo

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -10,6 +10,7 @@
 		  <div class="view">
 			<input class="toggle" type="checkbox" v-model="todo.completed">
 			<label @dblclick="editTodo(todo)">{{ todo.title }}</label>
+            <button class="destroy" @click="removeTodo(todo)"></button>
 		  </div>
 		  <input class="edit" type="text" v-model="todo.title" v-todo-focus="todo == editedTodo" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" v-on:keyup.27="cancelEdit(todo)">
 		</li>
@@ -49,6 +50,10 @@ export default {
     cancelEdit (todo) {
       this.editedTodo = null
       todo.title = this.beforeEditCache
+    },
+    removeTodo (todo) {
+      let index = this.todos.indexOf(todo)
+      this.todos.splice(index, 1)
     }
   },
   directives: {
